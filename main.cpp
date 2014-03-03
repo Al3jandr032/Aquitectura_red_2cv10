d7a4840d73fc7618996df022ff44b089908f33d0
furby32 <pedrolop15@hotmail.com>
2014-03-02 20:17:58 -0600

    Sent Ack
    Envia Ack en forma consecutiva

--- main.cpp
+++ main.cpp
@@ -1,25 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "canal.h"
 #include <iostream>
 #include <string>       
 #include <bitset>
 #include <string.h>
+#include <cstring>
 #include "protocolo.h"
+
 using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-//char prueba = 0x23;
-	/*
-	Armar la trama instanciando el tipo de trama a enviar y  configurandola
-	*/
-	char trama[100];
-	char a=0x01;
-	
+int main(int argc, char *argv[])
+{
+	char trama[2];
+	char Bloque=0x01;
 	Ack d;
-	d.setBloque(0x01);
-	trama[0]=d.getTipo();
-	trama[1]=d.getBloque();
-	cout <<trama<< '\n';
-
-	return 0;
+	inicializar();
+	do{
+		
+		d.setBloque(Bloque);
+		trama[0]=d.getTipo();
+		trama[1]=d.getBloque();
+		
+		
+		tx(trama,sizeof(trama));
+		
+		Bloque++;
+	}while(1);
+	terminar();
+	  
+    system("PAUSE");
+    return EXIT_SUCCESS;
 }

c7a0e57ae2c29f3bd41e1fba172c313fd85b86d2
furby32 <pedrolop15@hotmail.com>
2014-03-03 00:43:08 -0600

    Envia datos con el ultimo paquete
    el ultimo paquete de menos de los bytes determinados

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	//Trama a transmitir
 	char trama[7];
 	//contador
-	int cont=0;
+	int con,length;
 	//buffer para leer del archivo
 	char buffer[5];
 	//byte para numerar los paquetes
@@ -25,20 +25,33 @@ int main(int argc, char *argv[])
 	Data d;
 	// abrir el archivo en modo binario
 	ifstream myFile ("laravel.txt", ios::in | ios::binary);
-	
+	myFile.seekg (0, myFile.end);
+    length = myFile.tellg();
+    myFile.seekg (0, myFile.beg);
+    con=length;
 	inicializar();
-	
-		do{
-		myFile.read (buffer, 5);
-		d.setBloque(Bloque);
-		trama[0]=d.getTipo();
-		trama[1]=d.getBloque();
-		trama[2]='\0';
-		strncat(trama,buffer, 5);
-		tx(trama,sizeof(trama));
-
+	do{
+		if(con <= 5){
+			myFile.read(buffer, con);
+			d.setBloque(Bloque);
+			trama[0]=d.getTipo();
+			trama[1]=d.getBloque();
+			trama[2]='\0';
+			strncat(trama,buffer, con);
+			tx(trama,con+2);
+		}
+		else{
+			myFile.read(buffer, 5);
+			d.setBloque(Bloque);
+			trama[0]=d.getTipo();
+			trama[1]=d.getBloque();
+			trama[2]='\0';
+			strncat(trama,buffer, 5);
+			tx(trama,sizeof(trama));
+		}
 		Bloque++;
-	}while(1);
+		con=con-5;
+	}while(con > 0);
 		terminar();
  
     system("PAUSE");
